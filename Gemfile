c8648867ceaa6d4a20596df5f41a49bbfe6166b3
Ryan Miller <ryanmillergm@gmail.com>
2019-05-06 17:37:47 -0600

    Added tests for book, author, author_books Co-authored-by: Zach leach <35751823+zachlp2016@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ group :development, :test do
   gem 'launchy'
   gem 'pry'
   gem 'simplecov'
+  gem 'shoulda-matchers', '~> 3.1'
+  gem 'factory_bot_rails'
 end
 
 group :development do
---
+++ app/models/book.rb
@@ -0,0 +1,10 @@
+class Book < ApplicationRecord
+  has_many :author_books
+  has_many :authors, through: :author_books
+  has_many :reviews
+  validates_presence_of :title,
+                        :pages,
+                        :year_published,
+                        :book_img_url
+
+end
