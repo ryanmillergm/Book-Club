43f4e1dde33a2a0327aa75b01be177804cbc29bf
Zach Leach <zachleach@techoverco.com>
2019-05-07 13:26:02 -0600

    Verified tests are passing for author, book, and authorbook models.

--- db/migrate/20190507185725_create_author_books.rb
+++ db/migrate/20190507185725_create_author_books.rb
@@ -3,8 +3,6 @@ class CreateAuthorBooks < ActiveRecord::Migration[5.1]
     create_table :author_books do |t|
       t.references :author, foreign_key: true
       t.references :book, foreign_key: true
-
-      t.timestamps
     end
   end
 end
