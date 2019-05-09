fb7f1a281887021557da4ab04b9939cc74090263
Ryan Miller <ryanmillergm@gmail.com>
2019-05-09 12:49:52 -0600

    Add top_review test

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -20,17 +20,55 @@ RSpec.describe 'author show page', type: :feature do
       expect(page).to have_content('Name: Bill')
       expect(page).to_not have_content('Name: Jerry')
     end
+  end
 
+  it "Show page shows books by author with title, pages and years published" do
+    visit author_path(@author_1)
+
+    within "#book-id-#{@book_1.id}" do
+      expect(page).to have_xpath('//img[@src="google.com"]')
+      expect(page).to have_content('Title: Book_1')
+      expect(page).to have_content('Pages: 300')
+      expect(page).to have_content('Year Published: 1999')
+    end
+  end
+
+  it "Show page shows top review next to each book" do
+    visit author_path(@author_1)
+
+    within "#book-id-#{@book_1.id}" do
+      expect(page).to have_content('Top Review:')
+
+    # save_and_open_page
+    end
   end
 end
 
-# User Story 14
-# Author Show Page
+# User Story 4
+# All author names are links to a show page
+#
+# As a visitor
+# With the exception of an author's show page,
+# Anywhere I see an author's name on the site,
+# I can click on the name to go to that author's show page.
+
+# User Story 15
+# Author Show Page displays top review for each book
+# As a Visitor,
+# When I visit an author's show page,
+# Next to each book written by that author
+# I should see one of the highest rated reviews
+# (review should contain the title, score, and user name)
+
+# User Story 20
+# User can delete an author
 #
 # As a Visitor,
-# When I visit an author's show page
-# I see all book titles by that author
-# Each book should show its year of publication
-# Each book should show its number of pages
-# Each book should show a list of any other authors
-# (exclude this show page's author from that list)
+# When I visit an author's show page,
+# I see a link on the page to delete the author.
+# This link should return me to the book index page where I
+# no longer see this author listed.
+# If this author was the only author for any book, that book is also deleted.
+# If this author co-authored a book with someone else, that book should also be deleted, but not the other author.
+#
+# (you may need to delete other content before you can delete an author or book)
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Author, type: :model do
     it { should have_many :author_books }
     it { should have_many(:books).through(:author_books) }
   end
+
+  before :each do
+    @book_1 = Book.create!(title: "Book_1", pages: 300, year_published: 1999, book_img_url: "google.com")
+    @author_1 = Author.create!(name: "Bill")
+    @review_1 = Review.create!(title: "Review_1", rating: 3, text: "Review 1 description")
+    @review_2 = Review.create!(title: "Review_2", rating: 2, text: "Review 2 description")
+    @review_3 = Review.create!(title: "Review_3", rating: 4, text: "Review 3 description")
+    @book_1.reviews << @review_1
+    @book_1.reviews << @review_2
+    @book_1.reviews << @review_3
+    @author_1.books << @book_1
+  end
+
+  describe 'Instance methods' do
+    it 'top review for a book' do
+      expect = @book_1.top_review
+      expected = @review_3
+      expect(expect).to eq(expected)
+    end
+  end
 end
