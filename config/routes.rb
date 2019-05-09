b9b19d782650d7c61b0b64876ccd0ddc21f3074e
Ryan Miller <ryanmillergm@gmail.com>
2019-05-09 09:08:44 -0600

    Add books to author show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
-  
-  get '/books', to: 'books#index'
-  get '/books/show', to: 'books#show'
-  get '/authors', to: 'authors#index'
-  get '/authors/show', to: 'authors#show'
-  get '/authors', to: 'authors#index'
-  
+
+  resources :books, only:[:index, :show]
+  # get '/books', to: 'books#index'
+  # get '/book/show', to: 'book#show'
+  resources :authors, only:[:index, :show]
+  # get '/authors', to: 'authors#index'
+  # get '/authors/:id', to: 'authors#show'
+  # get '/authors', to: 'authors#index'
+
+
 end
