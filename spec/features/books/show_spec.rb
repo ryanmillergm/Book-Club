35636d83b5cbb551ec9899767d095925b09cb472
Zach Leach <zachleach@techoverco.com>
2019-05-08 23:55:14 -0600

    Added tests and sad path testing for new review and verified they are working.

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -11,10 +11,17 @@ RSpec.describe 'book show page', type: :feature do
     @author_1.books << @book_1
     @author_2.books << @book_2
     @author_3.books << @book_3
+    @user_1 = User.create!(name: "Henry")
+    @review_1 = @book_1.reviews.create!(title: "Review_1", rating: 3, text: "Review 1 description")
+    @review_2 = @book_1.reviews.create!(title: "Review_2", rating: 2, text: "Review 2 description")
+    @review_3 = @book_1.reviews.create!(title: "Review_3", rating: 4, text: "Review 3 description")
+    @user_1.reviews << @review_1
+    @user_1.reviews << @review_2
+    @user_1.reviews << @review_3
   end
 
   it 'Show page shows book and info' do
-    visit '/books/show'
+    visit book_path(@book_1)
 
     within "#book-id-#{@book_1.id}" do
       expect(page).to have_xpath('//img[@src="google.com"]')
@@ -23,6 +30,8 @@ RSpec.describe 'book show page', type: :feature do
       expect(page).to have_content('Year Published: 1999')
     end
 
+    visit book_path(@book_2)
+
     within "#book-id-#{@book_2.id}" do
       expect(page).to have_xpath('//img[@src="google.com"]')
       expect(page).to have_content('Title: Book_2')
@@ -30,11 +39,39 @@ RSpec.describe 'book show page', type: :feature do
       expect(page).to have_content('Year Published: 1999')
     end
 
+    visit book_path(@book_3)
+
     within "#book-id-#{@book_3.id}" do
       expect(page).to have_xpath('//img[@src="google.com"]')
       expect(page).to have_content('Title: Book_3')
       expect(page).to have_content('Pages: 300')
       expect(page).to have_content('Year Published: 1999')
+    end
+  end
+
+  it 'Shows a section where reviews for the book are shown' do
+
+    visit book_path(@book_1)
+
+    within "#review-id-#{@review_1.id}" do
+      expect(page).to have_content('Title: Review_1')
+      expect(page).to have_content('User: Henry')
+      expect(page).to have_content('Rating: 3')
+      expect(page).to have_content('Contents: Review 1 description')
+    end
+
+    within "#review-id-#{@review_2.id}" do
+      expect(page).to have_content('Title: Review_2')
+      expect(page).to have_content('User: Henry')
+      expect(page).to have_content('Rating: 2')
+      expect(page).to have_content('Contents: Review 2 description')
+    end
+
+    within "#review-id-#{@review_3.id}" do
+      expect(page).to have_content('Title: Review_3')
+      expect(page).to have_content('User: Henry')
+      expect(page).to have_content('Rating: 4')
+      expect(page).to have_content('Contents: Review 3 description')
     end
   end
 end
--- spec/features/reviews/new_review_spec.rb
+++ spec/features/reviews/new_review_spec.rb
@@ -1,38 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe 'A review can be successfully created' do
+  describe 'When a user clicks on adds a new review' do
+    before :each do
+      @book_1 = Book.create!(title: "Book_1", pages: 300, year_published: 1999, book_img_url: "google.com")
+    end
+    
+    it 'Creates a new review' do
+      visit book_path(@book_1)
 
-  before :each do
-    @book_1 = Book.create!(title: "Book_1", pages: 300, year_published: 1999, book_img_url: "google.com")
 
-    # @user_1 = User.create!(name: "Henry")
-    # @review_1 = @book_1.reviews.create!(title: "Review_1", rating: 3, text: "Review 1 description")
-    # @review_2 = @book_1.reviews.create!(title: "Review_2", rating: 2, text: "Review 2 description")
-    # @review_3 = @book_1.reviews.create!(title: "Review_3", rating: 4, text: "Review 3 description")
-    # @user_1.reviews << @review_1
-    # @user_1.reviews << @review_2
-    # @user_1.reviews << @review_3
+      within ".new-review-link" do
+        click_link('Add a Review')
+      end
+
+      expect(current_path).to eq(new_book_review_path(@book_1))
+
+      fill_in "Username", with: "lucky777"
+      fill_in "Title", with: "New Review"
+      fill_in "Rating", with: 3
+      fill_in "Text", with: "New review description."
+
+      click_on "Create Review"
+
+      new_review = @book_1.reviews.last
+
+      expect(new_review.title).to eq('New Review')
+      expect(new_review.username).to eq('Lucky777')
+      expect(current_path).to eq(book_path(@book_1))
+
+      visit book_path(@book_1)
+
+
+      within ".new-review-link" do
+        click_link('Add a Review')
+      end
+
+      fill_in "Username", with: "lucky777"
+      fill_in "Title", with: "New Review"
+      fill_in "Rating", with: 3
+      fill_in "Text", with: "Another New review description."
+
+      click_on "Create Review"
+
+      expect(@book_1.reviews.count).to eq(1)
+      expect(current_path).to eq(book_path(@book_1))
+    end
   end
-
-  it 'Can Create a new review' do
-    visit book_path(@book_1)
-
-    click_link('Add a Review')
-
-    expect(current_path).to eq(new_review_path(@book_1))
-
-    fill_in "Title", with: "New Review"
-    fill_in "Rating", with: 3
-    fill_in "Text", with: "New review description."
-
-    click_on "Create Review"
-
-    expect(current_path).to eq(book_path(@book))
-  end
-
-
-
-]
+end
 # On this new page, I see a form where I can enter:
 # - a review title
 # - a username as a string
